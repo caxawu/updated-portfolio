5e4c1df6da786d02daf0e8b88659d5e04c0c1564
Cathy Wu <caxawu@gmail.com>
2025-07-02 23:05:34 -0400

    1/2 about

--- src/staticPages/About.jsx
+++ src/staticPages/About.jsx
@@ -11,32 +11,43 @@ const About = () => {
   return (
     <div>
       <NavBar />
-      <div className='content'>
+      <div className='content' id="about">
         <div className='about-section' id='about-me'>
           <img src={profilePic} alt='Profile picture' loading='eager' />
-          <div className='text'>
-            <div className='highlight-pink text-secondary'>about me</div>
-            <div className='bio'>
-              I’m a designer who loves diving into complex UX challenges, transforming the complex web of user behavior, pain points, and opportunities into simple, meaningful, and delightful experiences.
-              I graduated from Dartmouth College with a degree in Cognitive Science focused in
-              <span className='bold'> Human-Computer Interaction</span> along with minors in
-              <span className='bold'> Human-Centered Design</span> and
-              <span className='bold'> Digital Arts</span>. I am currently a
-              <span className='bold'> UI/UX designer at Capital One.</span>
-              <div className='spacing-075'/>
-              I am driven by curiosity and love of learning, so I’m always picking up side projects and exploring different
-              creative outlets—whether that’s making art, pursuing my latest VR endeavor, or trying out a new recipe
-              in the kitchen.
+          <div className='text-bounding-box'>
+            <div className='color-box' id="color1">
+              <div className='bio'>
+                <div class="outline-overlay"></div>
+                <div className='text-secondary'>about me</div>
+                Hi there! I’m Cathy. I’m a designer who loves to chase wherever my creativity and curiosity take me. By day,
+                I’m a UX designer diving into complex problems and transforming them into simple, meaningful experiences.
+                By night, you’ll find me sketching, cooking new recipes, or obsessing over getting the interactions just
+                right in my latest VR project.
+                <br /><br />
+                I fell in love with design at Dartmouth College where I graduated with a degree in Cognitive Science focused in
+                <span className='bold highlight-dark-pink'> Human-Computer Interaction</span> along with minors in
+                <span className='bold highlight-dark-pink'> Human-Centered Design</span> and
+                <span className='bold highlight-dark-pink'> Digital Arts</span>. I'm currently a
+                <span className='bold highlight-dark-pink'> UI/UX designer at Capital One.</span>
+              </div>
+              </div>
+              <div className='color-box' id="color2">
+              <div className='bio'>
+                <div class="outline-overlay"></div>
+                <div className='text-secondary'>about my portfolio</div>
+                This journey started off with me as a designer, unable to find the motivation to update my portfolio. I’ve always struggled
+                to fit my work within a traditional portfolio format. So when I stumbled upon the amazing 3D portfolios out there, something clicked.
+                <br /><br />
+                I wanted to create my own interactive showcase—weaving together my love for design, 3D modeling, and game design. The freedom allowed
+                me to experiment and make my portfolio truly my own. The ideas were easy, though the coding definitely was not. But, seeing the room
+                I concepted, designed, and developed come together made it well worth it—and gave me the motivation to finally write a case study or two.
+                <br /><br />
+                I’ve had a ton of fun in the process. Thanks for taking the time to explore! ❤️
+              </div>
             </div>
-            <div className='spacing-3'/>
-            <div className='highlight-pink text-secondary'>about my portfolio</div>
-            <div className='bio'>
-              I was inspired to create my own interactive portfolio after discovering all the amazing 3D portfolios out there.
-              I wanted to creatively weave together my love for design, 3D modeling, and game design into something that would
-              showcase my work in a fun, interactive way. Having the creative freedom to design pushed my coding abilities but 
-              allowed me to experiment and make my portfolio truly my own. I’ve had a ton of fun in the process—thanks for 
-              taking the time to explore!
-            </div>
+          </div>
+          <div className='swatch'>
+            art doodles
           </div>
         </div>
       </div>

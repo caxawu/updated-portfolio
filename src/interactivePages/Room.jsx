4b9a11c0e42c005c94f36a491cdbda9b02fe7b72
Cathy Wu <caxawu@gmail.com>
2025-09-05 14:30:43 -0400

    change to published site instead of localhost

--- src/interactivePages/Room.jsx
+++ src/interactivePages/Room.jsx
@@ -42,8 +42,8 @@ const Room = ({ updateCameraPosition, updateCameraLookAt, defaultCamera, setFocu
   const [imgState, setImgState] = useState('none');
   const [animState, setAnimState] = useState('none');
   const [isArrowPressed, setIsArrowPressed] = useState(false);
-  // const [iframeSrc, setIframeSrc] = useState("https://xinaicathywu.me/static/case-studies");    // set screen1 when clicking VR buttons
-  const [iframeSrc, setIframeSrc] = useState("http://localhost:5173/static/case-studies");    // test link
+  const [iframeSrc, setIframeSrc] = useState("https://xinaicathywu.me/static/case-studies");    // set screen1 when clicking VR buttons
+  // const [iframeSrc, setIframeSrc] = useState("http://localhost:5173/static/case-studies");    // test link
 
   const particleSystemRef = useRef();
   const clickAudioRef = useRef(null);
